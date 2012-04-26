c448b20181eb81f70c2b82ac384f692f0cedd95d
Wraithan <xwraithanx@gmail.com>
2012-04-26 02:38:19 -0700

    have it graphing based on data from dailymile

--- training/core/models.py
+++ training/core/models.py
@@ -1,4 +1,11 @@
+from collections import defaultdict
+from datetime import datetime, timedelta
+import json
+import math
+from time import mktime
+
 from django.db import models
+import requests
 
 
 class DailyMileProfile(models.Model):
@@ -10,3 +17,26 @@ class DailyMileProfile(models.Model):
 
     def update_stats(self):
         pass
+
+    def stats_as_json(self):
+        retval = {'Cycling': {'real': 0, 'goal': 100},
+                  'Hiking': {'real': 0, 'goal': 5},
+                  'Running': {'real': 0, 'goal': 3}
+        }
+
+        api_endpoint='https://api.dailymile.com/'
+        seven_days_ago = mktime((datetime.now()-timedelta(days=7)).timetuple())
+        entries = json.loads(requests.get(
+            api_endpoint + 'people/me/entries.json',
+            params={
+                'oauth_token': self.access_token,
+                'since': seven_days_ago}
+        ).content)
+
+        for entry in entries['entries']:
+            if entry.has_key('workout'):
+                activity_type = entry['workout']['activity_type']
+                retval[activity_type]['real'] += entry['workout']['distance']['value']
+        for key,val in retval.items():
+            retval[key]['real'] = math.trunc(round(retval[key]['real']))
+        return retval
--- training/core/static/js/chart.js
+++ training/core/static/js/chart.js
@@ -5,24 +5,28 @@ var graph = function (data) {
         return Math.floor((value/max)*400);
     };
 
-    data.forEach(function(item) {
-        var max = Math.max(item.real, item.goal);
-        var label = document.createElement('p');
-        label.appendChild(document.createTextNode(item.type
-                                                  + " goal:"
-                                                  + item.goal
-                                                  +  " real:"
-                                                  + item.real));
-        chart.appendChild(label);
-        var real = document.createElement('div');
-        real.setAttribute('id', item.type + 'Bar');
-        real.setAttribute('class', 'real');
-        real.style.width = derive_width(item.real, max) + 'px';
-        label.appendChild(real);
-        var goal = document.createElement('div');
-        goal.setAttribute('class', 'goal');
-        goal.style.width = derive_width(item.goal, max) + 'px';
-        goal.appendChild(document.createTextNode('\u00a0'));
-        real.appendChild(goal);
-    });
+    for (var key in data) {
+        if (data.hasOwnProperty(key)) {
+            var item = data[key];
+            var max = Math.max(item.real, item.goal);
+            var label = document.createElement('p');
+            var real = document.createElement('div');
+            var goal = document.createElement('div');
+
+            label.appendChild(document.createTextNode(key
+                                                      + " goal:"
+                                                      + item.goal
+                                                      +  " real:"
+                                                      + item.real));
+            chart.appendChild(label);
+            real.setAttribute('id', item + 'Bar');
+            real.setAttribute('class', 'real');
+            real.style.width = derive_width(item.real, max) + 'px';
+            label.appendChild(real);
+            goal.setAttribute('class', 'goal');
+            goal.style.width = derive_width(item.goal, max) + 'px';
+            goal.appendChild(document.createTextNode('\u00a0'));
+            real.appendChild(goal);
+        }
+    }
 };

1a7948ed0baaec89e2fb4e220360291e516d55bc
Wraithan <xwraithanx@gmail.com>
2012-04-29 11:05:39 -0700

    moved static files

--- training/settings/__init__.py
+++ training/settings/__init__.py
@@ -87,7 +87,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
+STATIC_ROOT = os.path.join(SITE_ROOT, 'staticfiles')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
